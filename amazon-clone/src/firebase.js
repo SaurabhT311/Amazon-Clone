cfca7719f8c3da7b80b5607ccf26eeff9c13f39e
SaurabhT311 <tripathisaurabh.311@gmail.com>
2021-05-15 22:14:16 +0530

    Add: Added Header In Amazon_Clone

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAoI0eb_kvK1vzTXmkgE_5I4ymwoYeoAg8",
+    authDomain: "clone-98379.firebaseapp.com",
+    projectId: "clone-98379",
+    storageBucket: "clone-98379.appspot.com",
+    messagingSenderId: "99055193432",
+    appId: "1:99055193432:web:1a40f5f202faf1afc1e92d",
+    measurementId: "G-EW98HGHCL8"
+  };
